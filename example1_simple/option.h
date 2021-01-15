89bef3a7ee2c8ba96c59d4204f50153f604bbc10
kursnim <kursnim@gmail.com>
2021-01-15 23:41:49 +0900

    init

---
+++ example1_simple/option.h
@@ -0,0 +1,8 @@
+
+
+#define OPT_DEFINE          DEFINED_SOMETHING
+#define OPT_INT_TEST        (1)
+#define OPT_FLOAT_TEST      (1.5F)
+#define OPT_STRING_TEST     "Something"
+#define OPT_HEX_TEST        (0x001Fu)
+
